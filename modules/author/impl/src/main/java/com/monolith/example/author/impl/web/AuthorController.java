5fe2fe84ed2eacfb3fe0589c0286d19aba80f7f2
Dmytro Stepanyshchenko <mainalive@gmail.com>
2022-02-06 22:39:13 +0200

    New test report (#4)
    * Another test report
    
    * Add failed test
    
    * Fix build

--- modules/author/impl/src/main/java/com/monolith/example/author/impl/web/AuthorController.java
+++ modules/author/impl/src/main/java/com/monolith/example/author/impl/web/AuthorController.java
@@ -26,7 +26,7 @@ public class AuthorController {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     @ResponseStatus(HttpStatus.CREATED)
-    public AuthorCreatedResponse createReport(@RequestBody Author author) {
+    public AuthorCreatedResponse createAuthor(@RequestBody Author author) {
         var newAuthorId = authorService.registerNewAuthor(author);
         return AuthorCreatedResponse.builder()
                 .id(newAuthorId)
